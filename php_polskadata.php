3649012f5c793ab98d808d62822175cb821947ed
Cube Gear <72042040+cubegear@users.noreply.github.com>
2025-11-29 16:00:16 +0100

    Update php_polskadata.php

--- php_polskadata.php
+++ php_polskadata.php
@@ -23,70 +23,117 @@
         wykonanie 25'.
     </div>
 
-    <br><br>
+    <br><br><br>
 
 
     <?php
+        //instrukcja warunkowa prosta if
+
         $weekDaysPL = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"];
         $monthsPL = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
 
 
-        if(date("w") == 1) $weekDayPl = $weekDaysPL[0];
-        elseif(date("w") == 2) $weekDayPl = $weekDaysPL[1];
-        elseif(date("w") == 3) $weekDayPl = $weekDaysPL[2];
-        elseif(date("w") == 4) $weekDayPl = $weekDaysPL[3];
-        elseif(date("w") == 5) $weekDayPl = $weekDaysPL[4];
-        elseif(date("w") == 6) $weekDayPl = $weekDaysPL[5];
-        elseif(date("w") == 7) $weekDayPl = $weekDaysPL[6];
+        if(date("w") == 1) $weekDayPl1 = $weekDaysPL[0];
+        if(date("w") == 2) $weekDayPl1 = $weekDaysPL[1];
+        if(date("w") == 3) $weekDayPl1 = $weekDaysPL[2];
+        if(date("w") == 4) $weekDayPl1 = $weekDaysPL[3];
+        if(date("w") == 5) $weekDayPl1 = $weekDaysPL[4];
+        if(date("w") == 6) $weekDayPl1 = $weekDaysPL[5];
+        if(date("w") == 7) $weekDayPl1 = $weekDaysPL[6];
 
 
+
+        if(date("m") == 1) $monthPL1 = $monthsPL[0];
+        if(date("m") == 2) $monthPL1 = $monthsPL[1];
+        if(date("m") == 3) $monthPL1 = $monthsPL[2];
+        if(date("m") == 4) $monthPL1 = $monthsPL[3];
+        if(date("m") == 5) $monthPL1 = $monthsPL[4];
+        if(date("m") == 6) $monthPL1 = $monthsPL[5];
+        if(date("m") == 7) $monthPL1 = $monthsPL[6];
+        if(date("m") == 8) $monthPL1 = $monthsPL[7];
+        if(date("m") == 9) $monthPL1 = $monthsPL[8];
+        if(date("m") == 10) $monthPL1 = $monthsPL[9];
+        if(date("m") == 11) $monthPL1 = $monthsPL[10];
+        if(date("m") == 12) $monthPL1 = $monthsPL[11];
+
+
+        echo ("Dzisiejsza data to: <br> $weekDayPl1 " . date("d") . " $monthPL1 " . date("Y"));
+    ?>
+
+    <br><br><br><br>
+
+        <?php
+        //instrukcja switch...case
+
+        switch(date("w")) {
+            case 1:
+                $weekDayPl2 = $weekDaysPL[0];
+                break;
+            case 2:
+                $weekDayPl2 = $weekDaysPL[1];
+                break;
+            case 3:
+                $weekDayPl2 = $weekDaysPL[2];
+                break;
+            case 4:
+                $weekDayPl2 = $weekDaysPL[3];
+                break;
+            case 5:
+                $weekDayPl2 = $weekDaysPL[4];
+                break;
+            case 6:
+                $weekDayPl2 = $weekDaysPL[5];
+                break;
+            case 7:
+                $weekDayPl2 = $weekDaysPL[6];
+                break;
+            default:
+                $monthPL2 = "Nieznany";  
+        }
 
         switch (date("m")) {
             case 1: 
-                $monthPL = $monthsPL[0];
+                $monthPL2 = $monthsPL[0];
                 break;
             case 2:
-                $monthPL = $monthsPL[1];
+                $monthPL2 = $monthsPL[1];
                 break;
             case 3:
-                $monthPL = $monthsPL[2];
+                $monthPL2 = $monthsPL[2];
                 break;
             case 4:
-                $monthPL = $monthsPL[3];
+                $monthPL2 = $monthsPL[3];
                 break;
             case 5:
-                $monthPL = $monthsPL[4];
+                $monthPL2 = $monthsPL[4];
                 break;
             case 6:
-                $monthPL = $monthsPL[5];
+                $monthPL2 = $monthsPL[5];
                 break;
             case 7:
-                $monthPL = $monthsPL[6];
+                $monthPL2 = $monthsPL[6];
                 break;
             case 8:
-                $monthPL = $monthsPL[7];
+                $monthPL2 = $monthsPL[7];
                 break;
             case 9:
-                $monthPL = $monthsPL[8];
+                $monthPL2 = $monthsPL[8];
                 break;
             case 10:
-                $monthPL = $monthsPL[9];
+                $monthPL2 = $monthsPL[9];
                 break;
             case 11:
-                $monthPL = $monthsPL[10];
+                $monthPL2 = $monthsPL[10];
                 break;
             case 12:
-                $monthPL = $monthsPL[11];
+                $monthPL2 = $monthsPL[11];
                 break;
             default:
-                $monthPL = "Nieznany";
+                $monthPL2 = "Nieznany";
         }
 
 
-        echo ("Dzisiejsza data to: <br> $weekDayPl " . date("d") . " $monthPL " . date("Y"));
-
-        
-
+        echo ("Dzisiejsza data to: <br> $weekDayPl2 " . date("d") . " $monthPL2 " . date("Y"));
     ?>
 
 </body>
