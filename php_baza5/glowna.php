d39baf2f76df8e812b7cb2288a5018ccae28b8cb
Cube Gear <72042040+cubegear@users.noreply.github.com>
2026-03-13 16:18:17 +0100

    Add files via upload

---
+++ php_baza5/glowna.php
@@ -0,0 +1,266 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "baza5";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn) {
+    die("Wystąpił problem z połączeniem do serwera baz danych: ". mysqli_connect_error());
+}
+
+function sanitizeInput($input) {
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+};
+
+$errors = [];
+$formMessage = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["mainFormSubmit"])) {
+    $imie = isset($_POST["imie"]) ? sanitizeInput($_POST["imie"]) : "";
+    $nazwisko = isset($_POST["nazwisko"]) ? sanitizeInput($_POST["nazwisko"]) : "";
+    $wiek = isset($_POST["wiek"]) ? sanitizeInput($_POST["wiek"]) : "";
+    $plec = isset($_POST["plec"]) ? sanitizeInput($_POST["plec"]) : "";
+    $miasto = isset($_POST["miasto"]) ? sanitizeInput($_POST["miasto"]) : "";
+    $hobby = isset($_POST["hobby"]) ? implode(",", $_POST["hobby"]) : "";
+    $wyksztalcenie = isset($_POST["wyksztalcenie"]) ? sanitizeInput($_POST["wyksztalcenie"]) : "";
+
+    if(empty($imie)) $errors["imieError"] = "Pole nie może być puste!";
+    if(empty($nazwisko)) $errors["nazwiskoError"] = "Pole nie może być puste!";
+    if(empty($wiek)) $errors["wiekError"] = "Pole nie może być puste!";
+    if(empty($plec)) $errors["plecError"] = "Pole musi być wybrane!";
+    if(empty($miasto)) $errors["miastoError"] = "Pole nie może być puste!";
+    if(empty($hobby)) $errors["hobbyError"] = "Conajmnijej jedno pole musi być wybrane!!";
+    if(empty($wyksztalcenie) || $wyksztalcenie == "wybierz") $errors["wyksztalcenieError"] = "Pole musi być wybrane!";
+
+
+    if(empty($errors["imieError"]) && !preg_match('/^[A-ZĘÓĄŚŁŻŹĆ][a-zęóąśłżźć]*$/', $imie))
+        $errors["imieError"] = "Pole musi się składać tylko z liter(pierwsza duża reszta małe)!";
+    if(empty($errors["nazwiskoError"]) && !preg_match('/^[A-ZĘÓĄŚŁŻŹĆ][a-zęóąśłżźć]*$/', $nazwisko))
+        $errors["nazwiskoError"] = "Pole musi się składać tylko z liter(pierwsza duża reszta małe)!";
+    if(empty($errors["wiekError"]) && !preg_match('/^\d{1,3}$/',$wiek))
+        $errors["wiekError"] = "Pole musi się składać tylko z cyfr(max 3)!";
+    if(empty($errors["miastoError"]) && !preg_match('/^[A-ZĘÓĄŚŁŻŹĆ][a-zęóąśłżźć]*$/', $miasto))
+        $errors["miastoError"] = "Pole musi się składać tylko z liter(pierwsza duża reszta małe)!";
+
+    if(empty($errors)) {
+        $wiek_int = (int)$wiek;
+        $sql = "INSERT INTO osoba5 (imie, nazwisko, wiek, plec, miasto, hobby, wyksztalcenie) VALUES (?, ?, ?, ?, ?, ?, ?);";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "ssissss", $imie, $nazwisko, $wiek_int, $plec, $miasto, $hobby, $wyksztalcenie);
+
+        if(!mysqli_stmt_execute($stmt)){
+            die("Wystąpił problem z wykonaniem zapytania!");
+        }
+
+        mysqli_stmt_close($stmt);
+
+        $_SESSION["formMessage"] = "<span style='color:green'>Pomyślnie dodano rekord</span>";
+        header("Location: " . $_SERVER["PHP_SELF"]);
+        exit();
+
+
+
+    }
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["usunFormSubmit"])) {
+    $imieUsun = isset($_POST["imieUsun"]) ? sanitizeInput($_POST["imieUsun"]) : "";
+    $nazwiskoUsun = isset($_POST["nazwiskoUsun"]) ? sanitizeInput($_POST["nazwiskoUsun"]) : "";
+    $wiekUsun = isset($_POST["wiekUsun"]) ? sanitizeInput($_POST["wiekUsun"]) : "";
+    $plecUsun = isset($_POST["plecUsun"]) ? sanitizeInput($_POST["plecUsun"]) : "";
+    $miastoUsun = isset($_POST["miastoUsun"]) ? sanitizeInput($_POST["miastoUsun"]) : "";
+    $hobbyUsun = isset($_POST["hobbyUsun"]) ? sanitizeInput($_POST["hobbyUsun"]) : "";
+    $wyksztalcenieUsun = isset($_POST["wyksztalcenieUsun"]) ? sanitizeInput($_POST["wyksztalcenieUsun"]) : "";
+
+    
+    $wiekUsun_int = (int)$wiekUsun;
+    $sql = "DELETE FROM osoba5 WHERE imie = ? AND nazwisko = ? AND wiek = ? AND plec = ? AND  miasto  = ? AND hobby = ? AND wyksztalcenie = ? LIMIT 1;";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ssissss", $imieUsun, $nazwiskoUsun, $wiekUsun_int, $plecUsun, $miastoUsun, $hobbyUsun, $wyksztalcenieUsun);
+
+    if(!mysqli_stmt_execute($stmt)){
+        die("Wystąpił problem z wykonaniem zapytania!");
+    }
+
+    if(mysqli_stmt_affected_rows($stmt) == 1) {
+        $_SESSION["formMessage"] = "<span style='color:green'>Pomyślnie usunięto rekord</span>";
+        header("Location: " . $_SERVER["PHP_SELF"]);
+        exit();
+    } else {
+        $_SESSION["formMessage"] = "<span style='color:red'>Nie znaleziono </span>";
+        header("Location: " . $_SERVER["PHP_SELF"]);
+        exit();
+    }
+
+    mysqli_stmt_close($stmt);
+
+    
+}
+
+
+
+$sql = "SELECT * FROM osoba5";
+
+$result = mysqli_query($conn, $sql);
+
+if(!$result) {
+    die("Wystąpił problem przy wykonywaniu zapytania!");
+}
+
+
+if(isset($_SESSION["formMessage"])) {
+    $formMessage = $_SESSION["formMessage"];
+    unset($_SESSION["formMessage"]);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php_baza5</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div id="main">
+
+        <header>
+            <h1>DB - Spis ludności</h1>
+        </header>
+
+        <nav>
+            <a href="glowna_pdo.php">glowna_pdo</a>
+        </nav>
+
+        <section>
+            <form action="" method="post" name="mainForm" id="mainForm">
+                <label for="imie">Imię: </label>
+                <input type="text" name="imie" id="imie" value="<?php echo(isset($_POST['imie']) ? $_POST['imie'] : ''); ?>">
+                <br>
+                <span class="errors"><?php echo(isset($errors["imieError"]) ? $errors["imieError"] : ""); ?></span>
+                <br>
+
+                <label for="nazwisko">Nazwisko: </label>
+                <input type="text" name="nazwisko" id="nazwisko" value="<?php echo(isset($_POST['nazwisko']) ? $_POST['nazwisko'] : ''); ?>">
+                <br>
+                <span class="errors"><?php echo(isset($errors["nazwiskoError"]) ? $errors["nazwiskoError"] : ""); ?></span>
+                <br>
+
+                <label for="wiek">Wiek: </label>
+                <input type="text" name="wiek" id="wiek" value="<?php echo(isset($_POST['wiek']) ? $_POST['wiek'] : ''); ?>">
+                <br>
+                <span class="errors"><?php echo(isset($errors["wiekError"]) ? $errors["wiekError"] : ""); ?></span>
+                <br>
+
+                <label for="plec">Płeć: </label>
+                <input type="radio" name="plec" id="male" value="male" <?php echo(isset($_POST["plec"]) && $_POST["plec"] == "male" ? "checked" : ""); ?>>Mężczyzna
+                <input type="radio" name="plec" id="female" value="female" <?php echo(isset($_POST["plec"]) && $_POST["plec"] == "female" ? "checked" : ""); ?>>Kobieta
+                <br>
+                <span class="errors"><?php echo(isset($errors["plecError"]) ? $errors["plecError"] : ""); ?></span>
+                <br>
+                
+                <label for="miasto">Miasto: </label>
+                <input type="text" name="miasto" id="miasto" value="<?php echo(isset($_POST['miasto']) ? $_POST['miasto'] : ''); ?>">
+                <br>
+                <span class="errors"><?php echo(isset($errors["miastoError"]) ? $errors["miastoError"] : ""); ?></span>
+                <br>
+
+                <label for="hobby[]">Hobby:</label>
+                <br>
+                <input type="checkbox" name="hobby[]" id="jazdaRowerem" value="jazdaRowerem" <?php echo(isset($_POST["hobby"]) && in_array("jazdaRowerem", $_POST["hobby"]) ? "checked" : ""); ?>>jazdaRowerem
+                <br>
+                <input type="checkbox" name="hobby[]" id="czytanieKsiazekd" value="czytanieKsiazekd" <?php echo(isset($_POST["hobby"]) && in_array("czytanieKsiazekd", $_POST["hobby"]) ? "checked" : ""); ?>>czytanieKsiazekd
+                <br>
+                <input type="checkbox" name="hobby[]" id="granieWSapera" value="granieWSapera" <?php echo(isset($_POST["hobby"]) && in_array("granieWSapera", $_POST["hobby"]) ? "checked" : ""); ?>>granieWSapera
+                <br>
+                <span class="errors"><?php echo(isset($errors["hobbyError"]) ? $errors["hobbyError"] : ""); ?></span>
+                <br>
+
+                <label for="wyksztalcenie">Wykształcenie: </label>
+                <select name="wyksztalcenie" id="wyksztalcenie">
+                    <option value="wybierz" <?php echo(isset($_POST["wyksztalcenie"]) && $_POST["wyksztalcenie"] == "wybierz" ? "selected" : ""); ?> >Wybierz</option>
+                    <option value="podstawowe" <?php echo(isset($_POST["wyksztalcenie"]) && $_POST["wyksztalcenie"] == "podstawowe" ? "selected" : ""); ?> >Podstawowe</option>
+                    <option value="srednie" <?php echo(isset($_POST["wyksztalcenie"]) && $_POST["wyksztalcenie"] == "srednie" ? "selected" : ""); ?> >Średnie</option>
+                    <option value="wysokie" <?php echo(isset($_POST["wyksztalcenie"]) && $_POST["wyksztalcenie"] == "wysokie" ? "selected" : ""); ?> >Wysokie</option>
+                </select>
+                <br>
+                <span class="errors"><?php echo(isset($errors["wyksztalcenieError"]) ? $errors["wyksztalcenieError"] : ""); ?></span>
+                <br><br>
+
+                <input type="submit" value="Wyślij formularz" name="mainFormSubmit">
+                <input type="reset" value="Wyczyść formularz">
+                <?php echo(isset($formMessage) ? $formMessage : ""); ?>
+            </form>
+
+            <table>
+                <tr>
+                    <th>Imię</th>
+                    <th>Nazwisko</th>
+                    <th>Wiek</th>
+                    <th>Płeć</th>
+                    <th>Miasto</th>
+                    <th>Hobby</th>
+                    <th>Wykształcenie</th>
+                    <th>Usuń</th>
+                </tr>
+                <?php
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo("<tr>");
+                    echo("<td>{$row['imie']}</td>");
+                    echo("<td>{$row['nazwisko']}</td>");
+                    echo("<td>{$row['wiek']}</td>");
+                    echo("<td>{$row['plec']}</td>");
+                    echo("<td>{$row['miasto']}</td>");
+                    echo("<td>{$row['hobby']}</td>");
+                    echo("<td>{$row['wyksztalcenie']}</td>");
+                    echo("<td><form name='usunForm' id='usunForm' method='post'>");
+                    echo("<input type='hidden' value='" . $row['imie'] . "' name='imieUsun'>");
+                    echo("<input type='hidden' value='" . $row['nazwisko'] . "' name='nazwiskoUsun'>");
+                    echo("<input type='hidden' value='" . $row['wiek'] . "' name='wiekUsun'>");
+                    echo("<input type='hidden' value='" . $row['plec'] . "' name='plecUsun'>");
+                    echo("<input type='hidden' value='" . $row['miasto'] . "' name='miastoUsun'>");
+                    echo("<input type='hidden' value='" . $row['hobby'] . "' name='hobbyUsun'>");
+                    echo("<input type='hidden' value='" . $row['wyksztalcenie'] . "' name='wyksztalcenieUsun'>");
+                    echo("<input type='submit' value='Usuń' name='usunFormSubmit' onclick='return confirm(\"Czy jesteś pewny?\")'>");
+                    echo("</form></td>");
+                    echo("</tr>");
+                }
+                mysqli_close($conn);
+                ?>
+            </table>
+        </section>
+
+        <aside></aside>
+
+        <footer></footer>
+    </div>
+
+    <script>
+        function getId(id) {
+            return document.getElementById(id);
+        };
+
+        function firstLetterUp() {
+            this.value = this.value.replace(/\P{L}/gu, "");
+            this.value = this.value.slice(0,1).toUpperCase() + this.value.slice(1,).toLowerCase();
+        };
+
+        getId("imie").addEventListener("input", firstLetterUp);
+        getId("nazwisko").addEventListener("input", firstLetterUp);
+        getId("miasto").addEventListener("input", firstLetterUp);
+
+        getId("wiek").addEventListener("input", function() {
+            this.value = this.value.replace(/\D/g, "");
+            this.value = this.value.slice(0,3);
+        });
+    </script>
+</body>
+</html>
